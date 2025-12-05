796f6d09ebf2b39be142092ddd62e73b35079b87
Emarhnuel <ezeokekemm016@gmail.com>
2025-12-05 19:58:58 +0100

    feat(frontend): add custom favicon to Next.js document head

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="https://em-content.zobj.net/source/apple/391/jack-o-lantern_1f383.png"
+          type="image/png"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
